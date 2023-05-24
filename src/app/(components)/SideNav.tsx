add0e5b39c66b21c76c5f15e8d4ec2f1e1fad57b
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-24 15:06:23 -0400

    initial commit

---
+++ src/app/(components)/SideNav.tsx
@@ -0,0 +1,9 @@
+
+
+export default function SideNav(){
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function HomePage() {
+
+    return (
+        <>
+            <aside></aside>
+            <aside></aside>
+
+        </>
+    )
+}
